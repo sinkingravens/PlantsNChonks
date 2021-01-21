be6020b66b7baa7f4e7ea9d1b196acdc96491501
sethaire <sethairs@gmail.com>
2021-01-21 10:14:09 +0100

    Visar Viktor

--- PlantsNChonks/Assets/Scripts/PlayerController.cs
+++ PlantsNChonks/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(this);   
     }
 
     // Update is called once per frame
