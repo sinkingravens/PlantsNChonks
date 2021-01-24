3a8d0f4c008379988e68f12e0205026ab97415df
sethaire <sethairs@gmail.com>
2021-01-24 20:50:10 +0100

    A Test with Minions

---
+++ PlantsNChonks/Assets/Scripts/Minions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minions : MonoBehaviour
+{
+    public Transform Player;
+    public float speed = 3f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 displacement = Player.position - transform.position;
+        displacement = displacement.normalized;
+
+        if (Vector2.Distance(Player.position, transform.position) > 2.0f)
+        {
+            transform.position += (displacement * speed * Time.deltaTime);
+        }
+    }
+}
