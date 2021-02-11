8b9019938a73d02dafe01e3b610f6c3d2818813b
sethaire <sethairs@gmail.com>
2021-02-11 09:53:25 +0100

    Gun goes pew pew

--- PlantsNChonks/Assets/Scripts/GunsV2.cs
+++ PlantsNChonks/Assets/Scripts/GunsV2.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class GunsV2 : MonoBehaviour
 {
-    GameObject bullet;
-    Transform cursor;
+
+    [Header("Key Mechanics")]
+    public KeyCode shootKey = KeyCode.E;
     public float bulletSpeed = 30f;
+
+    public GameObject bullet;
+    public Transform firePoint;
 
     Vector2 cursorDirection;
     float cursorAngle;
 
     private void Update()
     {
+        InputProcess();
+    }
+
+    void InputProcess()
+    {
         cursorDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         cursorAngle = Mathf.Atan2(cursorDirection.y, cursorDirection.x) * Mathf.Rad2Deg;
 
-        //cursor.rotation = Quaternion.Euler
+        firePoint.rotation = Quaternion.Euler(0, 0, cursorAngle);
+        if(Input.GetKeyDown(shootKey))
+        {
+            GameObject bulletGoesPewPew = Instantiate(bullet);
+            bulletGoesPewPew.transform.position = firePoint.position;
+            bulletGoesPewPew.transform.rotation = Quaternion.Euler(0, 0, cursorAngle);
+
+            bulletGoesPewPew.GetComponent<Rigidbody2D>().velocity = firePoint.right * bulletSpeed; 
+        }
     }
 
 
