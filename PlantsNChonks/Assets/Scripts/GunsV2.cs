9a6061191d985a6fc55e914814b1606612614875
sethaire <sethairs@gmail.com>
2021-02-11 09:22:14 +0100

    I did new controller

---
+++ PlantsNChonks/Assets/Scripts/GunsV2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunsV2 : MonoBehaviour
+{
+    GameObject bullet;
+    Transform cursor;
+    public float bulletSpeed = 30f;
+
+    Vector2 cursorDirection;
+    float cursorAngle;
+
+    private void Update()
+    {
+        cursorDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorAngle = Mathf.Atan2(cursorDirection.y, cursorDirection.x) * Mathf.Rad2Deg;
+
+        //cursor.rotation = Quaternion.Euler
+    }
+
+
+}
---
+++ PlantsNChonks/Assets/Scripts/PlayerControllerV2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerV2 : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 3f;
+    public Rigidbody2D rb;
+    private Vector2 position;
+
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ProcessInput();   
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void ProcessInput()
+    {
+        //Processes the input
+        position.x = Input.GetAxisRaw("Horizontal");
+        position.y = Input.GetAxisRaw("Vertical");
+
+        //Sets the animators value to the correct ones
+        animator.SetFloat("Horizontal", position.x);
+        animator.SetFloat("Vertical", position.y);
+
+        //Makes sure when you walk between horizontal & vertical that its the same speed
+        position = new Vector2(position.x, position.y).normalized;
+
+    }
+
+    void Movement()
+    {
+        //Movement is in FixedUpdate because otherwise it would depend on framerates which would could potentially change movement
+        rb.velocity = new Vector2(position.x * moveSpeed, position.y * moveSpeed);
+    }
+}
