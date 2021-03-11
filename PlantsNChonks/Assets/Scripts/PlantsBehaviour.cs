86516bb29ffe4d4e92804fa38c392cad3e515c3e
sethaire <sethairs@gmail.com>
2021-03-11 09:21:35 +0100

    Added plant spawner etc

---
+++ PlantsNChonks/Assets/Scripts/PlantsBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantsBehaviour : MonoBehaviour
+{
+    public bool isHarvestable = false;
+    
+    //Clock
+    [SerializeField] private float plantTime;
+    [SerializeField] private int timeUntilHarvestable = 10; // Time until the plant is harvestable
+
+    // Update is called once per frame
+    void Update()
+    {
+        plantTimer();
+        canHarvest();
+
+    }
+
+    public void canHarvest()
+    {
+        if(timeUntilHarvestable == 0) //if timeUntilHarvestable is 0 then isHarvestabe = true;
+        {
+            isHarvestable = true;
+        }
+    }
+
+    public void plantTimer()
+    {
+        //if harvestable is false, continue the timer
+        if (isHarvestable == false)
+        {
+            plantTime += Time.deltaTime;
+            if (plantTime > 1) // if planttime is more than one, timeUntilHarvest will be one second less and plantTime will reset
+            {
+                timeUntilHarvestable -= 1;
+                plantTime = 0;
+            }
+        }
+    }
+}
---
+++ PlantsNChonks/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public int currentTimeIG = 0; // Shows the time inGame
+
+    private float currentTimePriv;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //currentTimePriv is always equal to Time.Delta time
+        currentTimePriv += Time.deltaTime;
+
+        //Until currenTimePriv = 1
+        if(currentTimePriv > 1)
+        {
+            //Then it will add a second to IG time and reset it to zero
+            currentTimeIG += 1;
+            currentTimePriv = 0;
+        }
+            
+    }
+}
